9f2b34103de415450e56801954a97b5167938b1d
rayangithub1 <rayanasif1080@gmail.com>
2026-05-11 17:09:46 +0500

    Update app.js

--- truthndare.app/public/app.js
+++ truthndare.app/public/app.js
@@ -444,6 +444,82 @@ function wrapText(
 // ==========================
 // DOWNLOAD IMAGE
 // ==========================
+// ==========================
+// DRAW ROUNDED RECTANGLE
+// ==========================
+function roundRect(ctx, x, y, width, height, radius) {
+
+  ctx.beginPath();
+
+  ctx.moveTo(x + radius, y);
+
+  ctx.lineTo(x + width - radius, y);
+  ctx.quadraticCurveTo(
+    x + width,
+    y,
+    x + width,
+    y + radius
+  );
+
+  ctx.lineTo(x + width, y + height - radius);
+
+  ctx.quadraticCurveTo(
+    x + width,
+    y + height,
+    x + width - radius,
+    y + height
+  );
+
+  ctx.lineTo(x + radius, y + height);
+
+  ctx.quadraticCurveTo(
+    x,
+    y + height,
+    x,
+    y + height - radius
+  );
+
+  ctx.lineTo(x, y + radius);
+
+  ctx.quadraticCurveTo(
+    x,
+    y,
+    x + radius,
+    y
+  );
+
+  ctx.closePath();
+
+}
+
+// ==========================
+// DRAW GLOW
+// ==========================
+function drawGlow(ctx, x, y, color, size) {
+
+  const glow =
+    ctx.createRadialGradient(
+      x,
+      y,
+      0,
+      x,
+      y,
+      size
+    );
+
+  glow.addColorStop(0, color);
+  glow.addColorStop(1, "transparent");
+
+  ctx.fillStyle = glow;
+
+  ctx.fillRect(
+    x - size,
+    y - size,
+    size * 2,
+    size * 2
+  );
+
+}
 function downloadImage(id, type, message) {
 
   const canvas =
